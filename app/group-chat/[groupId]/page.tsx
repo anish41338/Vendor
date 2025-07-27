f6b483a8997d8bb3eb331338352fe95c24752a28
anish41338 <anishsihag12@gmail.com>
2025-07-27 22:43:53 +0530

    new

--- app/group-chat/[groupId]/page.tsx
+++ app/group-chat/[groupId]/page.tsx
@@ -1,23 +1,23 @@
-// app/group-chat/[groupId]/page.tsx
 import ChatWindow from "@/components/chat/chat-window";
 
 interface PageProps {
-  params: {
+  params: Promise<{
     groupId: string;
-  };
+  }>;
 }
 
-export default  async function IndividualChatPage({ params }: PageProps) {
-  const groupId = parseInt(params.groupId, 10);
+export default async function IndividualChatPage({ params }: PageProps) {
+  const { groupId } = await params;
+  const groupIdNum = parseInt(groupId, 10);
 
-  if (isNaN(groupId)) {
+  if (isNaN(groupIdNum)) {
     return <div>Invalid Group ID</div>;
   }
 
   return (
     <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Group Chat Room</h1>
-        <ChatWindow groupId={groupId} />
+      <h1 className="text-2xl font-bold mb-4">Group Chat Room</h1>
+      <ChatWindow groupId={groupIdNum} />
     </div>
-  )
+  );
 }
